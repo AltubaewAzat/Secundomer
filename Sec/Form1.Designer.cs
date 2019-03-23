59cb09afd4f4bd1e847cb7a63ec6245357dcf2db
user <user@USER-PC>
2019-03-23 21:52:57 +0400

    Добавил удаление нескольких строк в listbox

--- Sec/Form1.Designer.cs
+++ Sec/Form1.Designer.cs
@@ -29,7 +29,6 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.btnStart = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
             this.lblMidValue = new System.Windows.Forms.Label();
@@ -40,20 +39,11 @@
             this.listBox = new System.Windows.Forms.ListBox();
             this.panel1 = new System.Windows.Forms.Panel();
             this.btnReset = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
             this.menuStrip1.SuspendLayout();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
-            // 
-            // btnStart
-            // 
-            this.btnStart.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.btnStart.Location = new System.Drawing.Point(158, 232);
-            this.btnStart.Name = "btnStart";
-            this.btnStart.Size = new System.Drawing.Size(130, 63);
-            this.btnStart.TabIndex = 0;
-            this.btnStart.Text = "Старт";
-            this.btnStart.UseVisualStyleBackColor = true;
-            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
             // 
             // label1
             // 
@@ -63,7 +53,7 @@
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(790, 196);
             this.label1.TabIndex = 1;
-            this.label1.Text = "00:00";
+            this.label1.Text = "00:00:00";
             this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // timer1
@@ -121,11 +111,15 @@
             this.listBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
             | System.Windows.Forms.AnchorStyles.Left) 
             | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBox.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.listBox.FormattingEnabled = true;
+            this.listBox.ItemHeight = 23;
             this.listBox.Location = new System.Drawing.Point(430, 232);
             this.listBox.Name = "listBox";
-            this.listBox.Size = new System.Drawing.Size(387, 173);
+            this.listBox.SelectionMode = System.Windows.Forms.SelectionMode.MultiExtended;
+            this.listBox.Size = new System.Drawing.Size(387, 165);
             this.listBox.TabIndex = 10;
+            this.listBox.TabStop = false;
             // 
             // panel1
             // 
@@ -144,21 +138,34 @@
             this.btnReset.Name = "btnReset";
             this.btnReset.Size = new System.Drawing.Size(88, 34);
             this.btnReset.TabIndex = 0;
+            this.btnReset.TabStop = false;
             this.btnReset.Text = "Reset";
             this.btnReset.UseVisualStyleBackColor = true;
             this.btnReset.Click += new System.EventHandler(this.btnReset_Click);
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.btnDelete.Location = new System.Drawing.Point(336, 269);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(88, 34);
+            this.btnDelete.TabIndex = 12;
+            this.btnDelete.TabStop = false;
+            this.btnDelete.Text = "Delete";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            this.btnDelete.Click += new System.EventHandler(this.btnDelete_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(827, 417);
+            this.Controls.Add(this.btnDelete);
             this.Controls.Add(this.panel1);
             this.Controls.Add(this.listBox);
             this.Controls.Add(this.lblMidValueHader);
             this.Controls.Add(this.lblMidValue);
             this.Controls.Add(this.btnReset);
-            this.Controls.Add(this.btnStart);
             this.Controls.Add(this.menuStrip1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.KeyPreview = true;
@@ -166,8 +173,7 @@
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Спидкубинг таймер";
-            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
-            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
@@ -179,7 +185,7 @@
 
         #endregion
 
-        private System.Windows.Forms.Button btnStart;
+        //private System.Windows.Forms.Button btnStart;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Timer timer1;
         private System.Windows.Forms.Label lblMidValue;
@@ -190,6 +196,8 @@
         private System.Windows.Forms.ListBox listBox;
         private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.Button btnDelete;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
     }
 }
 
--- Sec/Form1.cs
+++ Sec/Form1.cs
@@ -13,76 +13,45 @@ namespace Sec
     public partial class Form1 : Form
     {
         //start timer
-        private DateTime _dateStart;
+        private DateTime _dateStart; // время старта таймера
         //time array timers
-        private List<double> _times;
+        private List<double> _times; // сюда сохраняются интервалы времени     
         public Form1()
         {
             _dateStart = new DateTime();
-            _times = new List<double>();
+            _times = new List<double>();            
             InitializeComponent();
         }
-        private void timer1_Tick_1(object sender, EventArgs e) //тут таймер трудится
+
+        // во время работы таймера у нас:
+        // 1) работает метод GetTimeTimer(), который подсчитывает нам прошедшие миллисекунды
+        // 2) работает основное табло и показывает нам прошедшие секунды
+        private void timer1_Tick_1(object sender, EventArgs e)
         {
-            var timeTimer = GetTimeTimer();
-            label1.Text = ConvertToTextTime(timeTimer);
+            var timeTimer = GetTimeTimer(); // Дёргаем прошедшее время в миллисекундах
+            label1.Text = ConvertToTextTime(timeTimer); // конвертируем всё в string и засовываем в основное табло
         }
 
+        // конвертируем миллисекунды типа double в тип string, для того чтобы вывести их на listBox
         private string ConvertToTextTime(double time)
         {
-            int msec = Convert.ToInt32(time);
+            int msec = Convert.ToInt32(time); // воткнул все миллисекунды, преобразованные в int
             int sec = 0;
             int min = 0;
-            if (time < 1000.0)
+            if (time <= 100.0)
             {
                 msec = Convert.ToInt32(time);
             }
             else
             {
-                //if (second < 0)
-                //{
-                //    return "";
-                //}
-                //else
-                //{
-                //    if (second == 0)
-                //    {
-                //        return "00:00";
-                //    }
-                //    else
-                //    {
-                //        int minute = 0;
-                //        int hour = 0;
-
-                //        minute = second / 60;
-                //        hour = minute / 60;
-                //        minute %= 60;
-                //        second %= 60;
-
-                //        if (hour > 0)
-                //        {
-                //            return $"{hour:d2}:{minute:d2}:{second:d2}";
-                //        }
-                //        else
-                //        {
-                //            return $"{minute:d2}:{second:d2}";
-                //        }
-                //    }
-                //}                           
-
-                sec = msec / 1000;
+                sec = msec / 100;
                 min = sec / 60;
                 sec %= 60;
-                msec %= 1000;
-
-                //min = sec / 60;
-                //min %= 60;
-                //sec %= 60;
-                //msec %= 1000;
+                msec %= 100;
             }
 
             string msecText = msec.ToString();
-            while (msecText.Length < 3)
+            while (msecText.Length < 2)
             {
                 msecText = "0" + msecText;
             }
@@ -101,63 +70,43 @@ namespace Sec
             return $"{minText}:{secText}:{msecText}";
         }
 
+        // подсчитываем колличество миллисекунд, которое прошло от момента старта до финиша
         private double GetTimeTimer()
         {
             var start = _dateStart;
             var time = DateTime.Now - start;
-            return time.TotalMilliseconds;
+            return time.TotalMilliseconds / 10;
         }
 
         private void exitToolStripMenuItem_Click(object sender, EventArgs e)
         {
             this.Close();
-        }
-
-        private void btnStart_Click(object sender, EventArgs e)
-        {
-            try
-            {
-                //throw new ArgumentException("Текст Error");
-                bool workTimer = timer1.Enabled;
-                //таймер останавливаем
-                if (workTimer)
-                {
-                    StopTimer();
-                }
-                else
-                //запускаем таймер
-                {
-                    StartTimer();
-                }
-            }
-            catch (Exception exception)
-            {
-                MessageBox.Show(exception.ToString(), "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            }
-        }
-
+        }        
+               
         private void StartTimer()
         {
             btnReset.Enabled = false;
-            btnStart.Text = "Остановить";
-            _dateStart = DateTime.Now; //это я так обнуляю время
+            btnDelete.Enabled = false;            
+            _dateStart = DateTime.Now;
             timer1.Enabled = true;
         }
         private void StopTimer()
         {
             btnReset.Enabled = true;
-
-            timer1.Enabled = false;
-            btnStart.Text = "Старт";
+            btnDelete.Enabled = true;
+            timer1.Enabled = false;            
             var time = GetTimeTimer();
             _times.Add(time);
             var textTime = ConvertToTextTime(time);
-            label1.Text = ConvertToTextTime(0);
             listBox.Items.Add(textTime);
             lblMidValue.Text = ConvertToTextTime(GetTimeMiddle(_times));
             listBox.SelectedIndex = listBox.Items.Count - 1;
-        }
+            if (listBox.Items.Count > 1)
+            {
+                listBox.SetSelected(listBox.Items.Count -2, false);
+            }
 
+        }
         private double GetTimeMiddle(List<double> times)
         {
             if (times.Count == 0)
@@ -177,38 +126,66 @@ namespace Sec
                 return times.Sum() / Convert.ToDouble(times.Count);
             }
         }
-
-
-        private void btnTest_Click(object sender, EventArgs e)
-        {
-            label1.Text = ConvertToTextTime(500000.0);
-        }
-
-
-        private bool flagPress = false;
-
-        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
-        {
-            if (e.KeyChar == ' ')
-            {
-                btnStart_Click(null, null);
-            }
-        }
-
-        private void Form1_KeyDown(object sender, KeyEventArgs e)
-        {
-
-        }
-
-        private void Form1_KeyUp(object sender, KeyEventArgs e)
-        {
-        }
-
+        
         private void btnReset_Click(object sender, EventArgs e)
         {
             listBox.Items.Clear();
             _times.Clear();
-            lblMidValue.Text = ConvertToTextTime(0.0);
+            lblMidValue.Text = ConvertToTextTime(0);
+            this.listBox.Focus();                   
         }
+
+        private void Form1_Load(object Sender, EventArgs e)
+        {
+            //btnDelete.TabIndex = 0;
+            //label1.TabIndex = 1;
+            //listBox.TabIndex = 2;            
+            //btnReset.TabIndex = 3;                    
+        }
+
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int[] indexes = listBox.SelectedIndices.OfType<int>().OrderByDescending(i => i).ToArray();
+                foreach (int i in indexes)
+                {
+                    listBox.Items.RemoveAt(i);
+                    _times.RemoveAt(i);
+                    lblMidValue.Text = ConvertToTextTime(GetTimeMiddle(_times));
+                }
+                //var index = Convert.ToInt32(listBox.SelectedIndex);
+                //listBox.Items.RemoveAt(index);
+                //_times.RemoveAt(index);
+                //lblMidValue.Text = ConvertToTextTime(GetTimeMiddle(_times));
+                //this.listBox.Focus();
+            }
+            catch
+            {
+
+            }
+        }
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {            
+            if (e.KeyCode == Keys.Space)
+            {
+                try
+                {
+                    bool workTimer = timer1.Enabled;                    
+                    if (workTimer)
+                    {
+                        StopTimer();
+                    }
+                    else                    
+                    {
+                        StartTimer();
+                    }
+                }
+                catch (Exception exception)
+                {
+                    MessageBox.Show(exception.ToString(), "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }            
+        }       
     }
 }
